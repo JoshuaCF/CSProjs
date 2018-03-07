04c8c949bb3ccbfc4235ad24b50cc80dba93c215
Joshua <joshcade789@gmail.com>
2018-03-07 06:39:31 -0600

    Sync
    Leaving for school sync

--- Projects/1st6wks/data/banners.java
+++ Projects/1st6wks/data/banners.java
@@ -5,8 +5,11 @@ public class banners {
 	public static void main(String[] args) throws IOException {
 		//Declare variables
 		Scanner input = new Scanner(new FileReader("banners.dat"));
+		Scanner start = new Scanner(input.nextLine());
+		int[][] banners;
 		
 		//Start program
+		
 		
 		//Cleanup
 		input.close();
